a1883b31fb199ee338b3aef0503c46b687ea9d8e
Doug Waffle <dwaffle13154@alumni.focuscollege.com>
2020-12-09 20:07:57 -0800

    Updated signup component to be signup instead of just login component

--- src/components/pages/signup.tsx
+++ src/components/pages/signup.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Card, Form, Button} from 'react-bootstrap'
+import {Card, Form, Button, Row, Col} from 'react-bootstrap'
 import './style.scss'
 
 export default function SignupPage(){
@@ -12,7 +12,20 @@ export default function SignupPage(){
                     Sign Up:<br />
                     Please fill in the form to sign up.
                 </div>
+                <Form.Group className="name">
+                <Row >
+                    <Col>
+                        <Form.Label>First Name</Form.Label>
+                        <Form.Control type="firstname" placeholder="First Name" />
+                    </Col>
+                    <Col>
+                        <Form.Label className="lastname">Last Name</Form.Label>
+                        <Form.Control type="lastname" placeholder="Last Name" />
+                    </Col>
+                </Row>
+                </Form.Group>
                 <Form.Group className="username">
+                    
                     <Form.Label>Username:</Form.Label>
                     <Form.Control type="username" placeholder="Enter Username" />
                 </Form.Group>
