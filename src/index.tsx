de30a018c57372cdd0e094644db76b559159fd98
Khatab AL-DAGHISTANI <khatobi@hotmail.com>
2020-12-10 00:28:10 -0800

    Prepare platform for Organization page, add switch to site

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import ThePlatformWebsite from './ThePlatformWebSite'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThePlatformWebsite />
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/pages/OrganizationPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+//import Faq from '../components/OrganizationPage';
+import MainLayout from '../layouts/MainLayout';
+
+export default function OrganizationPage( props:{} ){
+
+    return <MainLayout>
+        
+        <h1> Organization Page </h1>
+        
+    </MainLayout>;
+
+}
