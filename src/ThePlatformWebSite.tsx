edecd557d22aec32c0ff948e72bc6feb795e4230
Derrick <dstone16640@alumni.focuscollege.com>
2020-12-11 13:34:27 -0800

    Changed ThePlatformWebsite to display article creation page

--- src/ThePlatformWebSite.tsx
+++ src/ThePlatformWebSite.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import Organization from './pages/OrganizationPage';
 import SignupPage from './pages/SignupPage';
+import {createNewArticle} from './components/ArticleList/ArticleInput/CreateArticle'
 
 import './App.scss';
 
@@ -15,6 +16,7 @@ export default function ThePlatformWebsite( props:{} ){
                 <Switch>
                     <Route path="/organization" component={Organization} />
                     <Route path="/signup" component={SignupPage} />
+                    <Route path="/articles" component={createNewArticle} />
                     <Route path="/" component={HomePage} />
                 </Switch>
             </BrowserRouter>
