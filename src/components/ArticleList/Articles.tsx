35af4781a151b7e752913ece7d11c5ab9b94509d
Derrick <dstone16640@alumni.focuscollege.com>
2020-12-11 12:07:12 -0800

    Added some pre-reqs for <Cards>

--- src/components/ArticleList/Articles.tsx
+++ src/components/ArticleList/Articles.tsx
@@ -1,17 +1,21 @@
 import React, { useEffect, useState } from 'react';
 
 export interface IArticle {
+    image?: string;
     title: number;
     author: string;
+    description: string[];
+    body: string[];
     category: string[];
     price?: number;
+    rating: number;
     series?: string;
     tag: string;
 }
 
 export interface ISearchFilter {
-    name?: string;
-    brand?: string;
+    title?: string;
+    tag?: string;
     category?: string;
 }
 
---
+++ src/components/ArticleList/HorizontalArticleList.tsx
@@ -0,0 +1,31 @@
+import React, { MouseEvent } from 'react'
+import { Badge, Button, Card } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import {IArticle} from './Articles'
+import './style.scss'
+
+
+
+export default function Article( props:IArticle ){
+
+    function onClickRoute() {
+        return function updateItemInCart(event:MouseEvent<HTMLButtonElement>)
+        {
+            const placeholder = 'do nothing'
+        }
+    }
+
+    return (
+        <Card>
+            <Card.Body>
+                <Card.Title> <Link to={`/Articles/${props.title}`}></Link></Card.Title>
+                <Card.Text>{props.description}</Card.Text>
+            </Card.Body>
+            <Card.Text>  <button onClick = {onClickRoute}> View Article </button> 
+            </Card.Text>
+
+            <Card.Footer>{props.price}, {props.category}, {props.tag}</Card.Footer>
+        </Card>
+    );
+
+}
