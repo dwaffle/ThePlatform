368ba06aa52f5fc565127c77ff1f89e0d9a0fe36
Khatab AL-DAGHISTANI <khatobi@hotmail.com>
2020-12-11 17:21:37 -0800

    Organization page, scaffolding

--- src/ThePlatformWebSite.tsx
+++ src/ThePlatformWebSite.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import HomePage from './pages/HomePage';
-import Organization from './pages/OrganizationPage';
+import Organization from './pages/Organization/OrganizationPage';
 
 import './App.scss';
 
