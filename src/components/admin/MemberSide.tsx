49ca52150229b496f8247cc82560b4b84ec03829
Khatab AL-DAGHISTANI <khatobi@hotmail.com>
2020-12-16 17:34:36 -0800

    admin page 1st, design

---
+++ src/components/admin/MemberSide.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Row, Col, CardDeck, Card } from 'react-bootstrap';
+
+export default function MemberSide( props:{} ){
+ 
+
+    return <>
+            <Row>
+                <Col>MemberList </Col>
+            </Row>
+
+            <Row>
+                <Col>Ban Member</Col>
+            </Row>
+
+            <Row>
+                <Col>Ban Reason Box</Col>
+            </Row>
+
+            <Row>
+                <Col>UnBan Member</Col>
+            </Row>
+    </>;
+
+}
---
+++ src/components/admin/OrganizationSide.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Row, Col, CardDeck, Card } from 'react-bootstrap';
+
+export default function OrganizationSide( props:{} ){
+ 
+
+    return <>
+            <Row>
+                <Col>OrganizationList </Col>
+            </Row>
+
+            <Row>
+                <Col>Ban Organization</Col>
+            </Row>
+
+            <Row>
+                <Col>Ban Reason Box</Col>
+            </Row>
+
+            <Row>
+                <Col>UnBan Member</Col>
+            </Row>
+    </>;
+
+}
