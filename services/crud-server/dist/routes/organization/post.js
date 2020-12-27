0ef19d04023ecaeba77a172f00c4afa2c9a88e10
Derrick <dstone16640@alumni.focuscollege.com>
2020-12-26 16:40:26 -0800

    Deleted Dist from crud-server

--- services/crud-server/dist/routes/organization/post.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.post = void 0;
-function post(app) {
-}
-exports.post = post;
-//# sourceMappingURL=post.js.map
