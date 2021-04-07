60b756f69f52005a708cc3b7e33fd54679d9131a
Doug Waffle <dwaffle13154@alumni.focuscollege.com>
2021-04-07 13:34:56 -0700

    Added organizations to the trending/latest display tables.  Moved tables into the horizontal organization list to minimize DB calls

--- src/pages/Organization/OrganizationPage.tsx
+++ src/pages/Organization/OrganizationPage.tsx
@@ -3,14 +3,12 @@ import React from 'react';
 //import Faq from '../components/OrganizationPage';
 import MainLayout from '../../layouts/MainLayout';
 import HorizontalOrganizationList from '../../components/organization/HorizontalOrganizationList';
-import TrendingOrganization from '../../components/organization/TrendingOrganization';
 // import './style.scss'
 
 export default function OrganizationPage(props: {}) {
   return (
     <MainLayout>
       <HorizontalOrganizationList />
-      <TrendingOrganization />
     </MainLayout>
   );
 }
