ee0f54ab284f38e42f0813b2391e127d5c2dee86
Derrick <dstone16640@alumni.focuscollege.com>
2021-04-29 20:30:35 -0700

    Commit to pull changes

--- src/components/homePage/HomePage.tsx
+++ src/components/homePage/HomePage.tsx
@@ -9,17 +9,22 @@ import './style.scss';
 export default function HomePage(props: {}) {
   // all series
   const seriesList = useRecoilValue<ISeries[]>(seriesListState);
+  //  const test = seriesList.sort((a,b) => new Date(b.ser_creationDate).getTime() - new Date(a.date).getTime())[0];
   // all articles
   const { articleList, setArticleList } = useArticleList();
 
-  // const dateNow = new Date();
-  // console.log('datenow', dateNow);
+  const dateNow = new Date();
+  console.log('datenow', dateNow);
 
   //find the newest series
-  const newestSeries = seriesList[seriesList.length - 1];
+  //not finding the latest, yet.
+  const newestSeries = seriesList.find((s) => s.ser_creationDate);
+  console.log('newest series? ', newestSeries?.ser_creationDate);
 
-  // gets newest article
-  const newestArticle = articleList[articleList.length - 1];
+  //not finding the latest, yet.
+  const newestArticle = articleList.find((a) => a.art_creationDate);
+  // // console.log("newest Article?", newestArticle)
+  // const last artID
   
   // const latestSeries = seriesList.sort((objA, objB) => objA.ser_creationDate.getTime() - dateNow;
   return (
@@ -28,10 +33,7 @@ export default function HomePage(props: {}) {
         <Col xs={8}>
           {' '}
           <div className="HeadlineArticle">
-            <h1> {newestArticle.art_title} </h1>
-            <p>
-              written by:{newestArticle.user_userName}
-            </p>
+            <h1> HeadlineArticle </h1>
           </div>{' '}
         </Col>
         <Col>
